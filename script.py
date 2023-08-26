87ba0986f72018b34f099682e29026d0f71e8c11
T.D MORAPEDI <101623837+T-MORAPEDI@users.noreply.github.com>
2023-08-26 10:19:34 +0200

    Update script.py

--- script.py
+++ script.py
@@ -2,11 +2,14 @@ import pandas as pd
 import sklearn.linear_model as linear_model
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.feature_selection import
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("tourism_data_500_points.csv", sep=";")
 
-X_train, X_test, y_train, y_test = 
+X =
+y =
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 model_lasso = linear_model.Lasso(alpha=0.5)
 model_RF = RandomForestRegressor(max_depth=2, random_state=0)
